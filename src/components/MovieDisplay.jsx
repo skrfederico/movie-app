08047041a5b836845b50000abfdb5c7130267b69
matthewkolden <matthewkolden2k@gmail.com>
2023-03-18 08:28:13 -0400

    Add additional carousel styling

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -12,14 +12,17 @@ export function MovieDisplay() {
   const Loaded = ({ movie }) => {
     return (
       <Link to={`/${movie.title}`}>
-        <div className="flex flex-col justify-center items-center p-2 h-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+        <div className="group flex flex-col justify-center items-center p-2 h-full transition ease-in-out delay-150 group/item hover:-translate-y-4 hover:scale-110 duration-300">
           <img
-            className="object-contain w-full rounded-3xl"
+            className="object-contain w-full rounded-3xl group-hover:brightness-50"
             src={movie.poster}
             alt={movie.title}
           />
+          <div class="opacity-0 group-hover:opacity-100 group-hover:brightness-100 duration-300 absolute inset-0 z-10 flex flex-col justify-end items-start  px-8 py-24">
+            <p className="text-2xl text-white font-semibold">{movie.title}</p>
+            <p>({movie.rating})</p>
+          </div>
         </div>
-        <h2 className="">{movie.title}</h2>
       </Link>
     )
   }
