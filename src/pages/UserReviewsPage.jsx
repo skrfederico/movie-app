8d76e0e6186e65ccbec8bd6404786d0607b96690
matthewkolden <matthewkolden2k@gmail.com>
2023-03-18 04:32:05 -0400

    Create reviews page

---
+++ src/pages/UserReviewsPage.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import { useController } from '../Controller'
+import Review from '../components/Review'
+
+import { moviePageClasses } from '../appClasses'
+
+export function UserReviewPage({ user }) {
+  const { getAllReviews, reviews } = useController()
+
+  useEffect(() => {
+    getAllReviews()
+  }, [])
+
+  return (
+    <div className={moviePageClasses.container}>
+      <div className="flex flex-col sm:w-3/4 md:w-3/4 mx-auto">
+        <div className={moviePageClasses.reviewsContainer}>
+          <h2 className="text-2xl font-semibold mb-4">Your Reviews</h2>
+          <div className={moviePageClasses.reviewListContainer}>
+            {reviews && reviews.length > 0 && (
+              <>
+                {reviews.map((review, i) => {
+                  if (review.user === user._id)
+                    return <Review key={i} review={review} />
+                })}
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
