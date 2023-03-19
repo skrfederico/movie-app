e197cb647eb9c97668a1469164044ab32a41ffa4
matthewkolden <matthewkolden2k@gmail.com>
2023-03-19 17:49:35 -0400

    Dawn styles

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import * as usersService from '../services/usersService'
+import '../index.css'
 
 export default function LoginForm({ setUser }) {
   const [credentials, setCredentials] = useState({
@@ -28,31 +29,58 @@ export default function LoginForm({ setUser }) {
   }
 
   return (
-    <div>
-      <div className="form-container">
-        <form autoComplete="off" onSubmit={handleSubmit}>
-          <label>Email</label>
-          <input
-            type="text"
-            className="border p-2 bg-slate-800"
-            name="email"
-            value={credentials.email}
-            onChange={handleChange}
-            required
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            className="border p-2 bg-slate-800"
-            name="password"
-            value={credentials.password}
-            onChange={handleChange}
-            required
-          />
-          <button type="submit">LOG IN</button>
-        </form>
+    <body class=" w-full h-full login-page">
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <h2 className="text-4xl font-extrabold mb-16  ">
+            Movie Review Login
+          </h2>
+          <form
+            className="border-8 border-white-1000 rounded-md p-12 w-50 text-White  "
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <div className="mb-4 w-full font-extrabold text-2xl">
+              <label htmlFor="email" className="block mb-2">
+                Email
+              </label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={credentials.email}
+                onChange={handleChange}
+                required
+                className="border p-2 bg-slate-800 border-slate-700"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-8 w-full font-extrabold text-2xl">
+              <label htmlFor="password" className="block mb-2">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={credentials.password}
+                onChange={handleChange}
+                required
+                className="border p-2 bg-slate-800 border-slate-700"
+                placeholder="Password"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded-md shadow hover:bg-blue-600 transition-colors duration-300 mt-4"
+            >
+              Log in
+            </button>
+            <p className="text-red-500 text-sm mt-4">{error}</p>
+          </form>
+        </div>
       </div>
-      <p className="error-message">&nbsp;{error}</p>
-    </div>
+    </body>
   )
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react'
 import { signUp } from '../services/usersService'
+import { Component } from 'react'
 
 export default class SignUpForm extends Component {
   state = {
@@ -41,50 +41,78 @@ export default class SignUpForm extends Component {
   // (its job is to return the UI)
   render() {
     const disable = this.state.password !== this.state.confirm
+
+    // document.body.classList.add("signup-page");
+
     return (
-      <div className="form-container">
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <label>Name</label>
-          <input
-            type="text"
-            className="border p-2 bg-slate-800"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            required
-          />
-          <label>Email</label>
-          <input
-            type="email"
-            className="border p-2 bg-slate-800"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            required
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            className="border p-2 bg-slate-800"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
-          <label>Confirm</label>
-          <input
-            type="password"
-            className="border p-2 bg-slate-800"
-            name="confirm"
-            value={this.state.confirm}
-            onChange={this.handleChange}
-            required
-          />
-          <button type="submit" disabled={disable}>
-            SIGN UP
-          </button>
-          <p className="error-message">&nbsp;{this.state.error}</p>
-        </form>
+      <div className=" w-full h-full signup-page">
+        <div className="flex items-center justify-center h-screen">
+          <div className="flex flex-col items-center justify-center min-h-screen">
+            <h2 className="text-4xl font-extrabold mb-16 ">
+              Register to Sign Up
+            </h2>
+            <div className="border-8 border-white-1000 rounded-md p-12 w-50 text-White">
+              <form autoComplete="off" onSubmit={this.handleSubmit}>
+                <div className="flex flex-col">
+                  <label className="mb-4 w-full font-extrabold text-2xl">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="border p-2 bg-slate-800 border-slate-700"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                    required
+                  />
+                  <label className="mb-4 w-full font-extrabold text-2xl">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="border p-2 bg-slate-800 border-slate-700"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    required
+                  />
+                  <label className="mb-4 w-full font-extrabold text-2xl">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="border p-2 bg-slate-800 border-slate-700"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    required
+                  />
+                  <label className="mb-4 w-full font-extrabold text-2xl">
+                    Confirm
+                  </label>
+                  <input
+                    type="password"
+                    className="border p-2 bg-slate-800 border-slate-700"
+                    name="confirm"
+                    value={this.state.confirm}
+                    onChange={this.handleChange}
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white py-2 px-4 rounded-md shadow hover:bg-blue-600 transition-colors duration-300 mt-4"
+                  disabled={disable}
+                >
+                  Sign Up
+                </button>
+                <p className="text-red-500 text-sm mt-2">
+                  &nbsp;{this.state.error}
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
